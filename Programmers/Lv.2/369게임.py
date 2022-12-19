6d86b588972ad6b1f1d9e91531449ab0cc2b1601
hyerimmy <heyrim2010@naver.com>
2022-12-20 01:11:14 +0900

    2022.12.19. mon

---
+++ Programmers/Lv.2/369게임.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    clap = ['3','6','9']
+
+    for o in str(order):
+        if o in clap:
+            answer += 1
+    return answer
+
+print(solution(29423))
