d958d361075a3b5ea0fda6e80e32f7b1443a1a2a
hyerimmy <heyrim2010@naver.com>
2023-01-25 00:28:40 +0900

    2023.01.24. tue

--- Programmers/Lv.0/삼각형의 완성조건 (2).py
+++ Programmers/Lv.0/삼각형의 완성조건 (2).py
@@ -1,13 +1,12 @@
 def solution(sides):
     answer = list()
     sides.sort()
-    for a in range(sides[1] - sides[0] + 1, sides[1]):
+    for a in range(sides[1] - sides[0] + 1, sides[1]+1):
         if a not in answer:
             answer.append(a)
-    for b in range(sides[0] + 1, sides[0] + sides[1]):
+    for b in range(sides[1]+1, sides[0] + sides[1]):
         if b not in answer:
             answer.append(b)
     return len(answer)
-
 
 print(solution([11, 7]))
