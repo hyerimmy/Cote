d04a8e3b2f0b3fa5e8104d4580d7ec7f85ad6a21
hyerimmy <heyrim2010@naver.com>
2023-01-29 01:24:53 +0900

    2023.01.28. sat

--- Programmers/Lv.2/[미완료 91.7점] 전화번호 목록.py
+++ Programmers/Lv.2/[미완료 91.7점] 전화번호 목록.py
@@ -1,24 +1,15 @@
 def solution(phone_book):
-    answer = True
-
-    phone_book.sort(key=len)
-
-    for pb in range(0,len(phone_book)):
-        for npb in range(pb+1, len(phone_book)):
-            # if phone_book[npb] != phone_book[pb] \
-            #         and len(phone_book[npb]) >= len(phone_book[pb]):
-                # print("phone_book[pb]",phone_book[pb])
-                # print("npb",npb)
-                #
-                # print("phone_book[pb][0:3]",phone_book[pb][0:len(phone_book[pb])])
-                # print("npb[0:3]",npb[0:len(phone_book[pb])])
-                #
-                # print(phone_book[pb][0:len(phone_book[pb])] == npb[0:len(phone_book[pb])])
-                answer = phone_book[pb][0:len(phone_book[pb])] != phone_book[npb][0:len(phone_book[pb])]
-                if not answer:
-                    return answer
+    hash = []
+    for pb in phone_book:
+        for i in range(1,len(pb)):
+            hash.append(pb[0:i])
+    print(hash)
+    for pb in phone_book:
+        if pb in hash:
+            return False
     return True
 
-print(solution(["119", "97674223", "1195524421"]	))
+# print(solution(["119", "97674223", "1195524421"]	))
 print(solution(["123", "456", "789"]))
-print(solution(["12","123","1235","567","88"]))
+# print(solution(["12","123","1235","567","88"]))
+# print(solution(["1","12"]))
