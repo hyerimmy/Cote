effb84a1e440da3e9b43713164d6c19c4451e245
hyerimmy <heyrim2010@naver.com>
2023-08-11 18:15:30 +0900

    [refactor] 파일 구조 정리

--- Code/Solution.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public static void main(String[] args) {
-        System.out.println(solution("ㄴ"));
-    }
-    public static String solution(String s) {
-        return s;
-    }
-}
