30d57e7d5c5812398130f0dd657d0411eaf5a95d
hyerimmy <heyrim2010@naver.com>
2023-08-11 17:14:53 +0900

    [fix] 자바 모듈로 수정

---
+++ Code/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
